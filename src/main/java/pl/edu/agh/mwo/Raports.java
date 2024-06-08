86ab795981da7002a8de70bb23a1dffca34f1261
pjachera <pjachera@gmail.com>
2024-06-08 13:36:23 +0200

    generate function in Raports

--- src/main/java/pl/edu/agh/mwo/Raports.java
+++ src/main/java/pl/edu/agh/mwo/Raports.java
@@ -1,16 +1,22 @@
 package pl.edu.agh.mwo;
 
+import org.jcp.xml.dsig.internal.dom.DOMSubTreeData;
+
 public class Raports {
 
     Model model;
-
 
     public Raports(Model model) {
         this.model = model;
     }
 
     void generate(){
-
+          for(Project project : model.getAllProjects()){
+              System.out.println(project.name);
+                 for(LogItem logItem : project.getTasks()) {
+                     System.out.println(logItem.taskName + logItem.startDateTime + logItem.stopDateTime);
+                 }
+          }
          
     }
 }
