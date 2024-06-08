e15c11284c269d57c3cba2a737faecb8df5853e9
Piotr <pjachera@gmail.com>
2024-06-08 19:07:05 +0200

    new class Raports

--- src/main/java/pl/edu/agh/mwo/Raports.java
+++ src/main/java/pl/edu/agh/mwo/Raports.java
@@ -5,42 +5,32 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Period;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class Raports {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
-        LogItem logItems = new LogItem("Log");
-        LogItem logItems2 = new LogItem("Log2");
-        LogItem logItems3 = new LogItem("Log3");
 
-
-
+        Model model = new Model();
         Project project = new Project("Project");
-        project.addTask(logItems.getTaskName());
-        project.addTask(logItems2.getTaskName());
-        project.addTask(logItems3.getTaskName());
-
-
         Project project2 = new Project("Project2");
-        project2.addTask(logItems.getTaskName());
-        project2.addTask(logItems2.getTaskName());
-        project2.addTask(logItems3.getTaskName());
 
-        List<Project> projectList = new ArrayList<>();
+        project.addTask("Task 1");
+        project.addTask("Task 2");
+        project.addTask("Task 3");
 
-        projectList.add(project);
-        projectList.add(project2);
+        project2.addTask("Task 4");
+        project2.addTask("Task 5");
+        project2.addTask("Task 6");
 
-
-        Model model = new Model(projectList);
+        model.addProject(project);
+        model.addProject(project2);
 
         Raports raports = new Raports(model);
+
+        raports.generateFromFile();
 
         raports.generate();
     }
@@ -51,30 +41,32 @@ public class Raports {
         this.model = model;
     }
 
-    HashMap<LogItem, Double> timeWork = new HashMap<>();
+    //  HashMap<LogItem, Double> timeWork = new HashMap<>();
 
     void generate(){
 
-          for(Project project : model.getAllProjects()){
-              System.out.println("Nazwa projektu : " + project.getName());
-                 for(LogItem logItem : project.getTasks()) {
+        for(Project project : model.getProjects()){
+            System.out.println("Nazwa projektu : " + project.getName());
+            for(String task : project.getTasks()) {
+                System.out.println("Nazwa zadania: " + task);
+            }
+        }
 
-                     System.out.println("Nazwa zadania: " + logItem.taskName + "\n" + "Data rorzpoczecia: " + logItem.startDateTime + "\n"+ "Data zakonczenia: " + "2024-06-09T14:47:58.010897475"); //logItem.stopDateTime
-                     long timeInSeconds = WorkTime(logItem.startDateTime, "2024-06-09T14:47:58.010897475").getSeconds();
-                     System.out.println(timeInSeconds);
+    }
 
-                     timeWork.put(logItem, Double.valueOf(timeInSeconds));
-                    // timeInSeconds += timeWork.get(logItem.taskName);
+    void generateFromFile(){
+        CsvReader csvReader = new CsvReader();
+        Map<String, List<LogItem>> projectList = csvReader.readFile();
 
+        for(String projectName : projectList.keySet()){
+            for(LogItem logItem : projectList.get(projectName)){
+                System.out.println(logItem.toString());
+            }
+        }
 
-                     System.out.println(timeWork.get(logItem).toString());
-                 }
-          }
-         
     }
 
     Duration WorkTime(String start, String end){
         return Duration.between(LocalDateTime.parse(start),LocalDateTime.parse(end));
     }
 }
-
