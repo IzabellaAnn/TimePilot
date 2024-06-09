acd3986b9c13dae794e26d1bb1b41d67a62b122c
IzabellaAnn <49371406+IzabellaAnn@users.noreply.github.com>
2024-06-09 13:27:06 +0200

    Update App.java
    git help in case someone put incorrect command

--- src/main/java/pl/edu/agh/mwo/App.java
+++ src/main/java/pl/edu/agh/mwo/App.java
@@ -1,29 +1,28 @@
 package pl.edu.agh.mwo;
-
 import org.apache.commons.cli.*;
-
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class App {
-
-    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss");
     public static void main(String[] args) {
         Options options = new Options();
         Option startOption = new Option("start", false, "activate start application");
         Option stopOption = new Option("stop", false, "activate stop application");
+        Option helpOption = new Option("help", false, "display this help");
+
         Option projectOption = new Option("p","project", true, "project name");
         Option taskOption = new Option("t","task", true, "task name");
+
         options.addOption(startOption);
         options.addOption(stopOption);
         options.addOption(projectOption);
         options.addOption(taskOption);
+        options.addOption(helpOption);
+
         CommandLineParser parser = new DefaultParser();
         HelpFormatter formatter = new HelpFormatter();
 
         try {
-            // parse the command line arguments
             CommandLine line = parser.parse(options, args);
 
             // Check if help is requested
@@ -44,9 +43,9 @@ public class App {
                 CsvReader csvReader = new CsvReader();
                 PrintWriter printWriter = new PrintWriter();
                 Project project = new Project(projectNameCmd);
-             //   project.addTask(line.getOptionValue("t"), LocalDateTime.now().toString(), LocalDateTime.now().toString());
+             // Project.addTask(line.getOptionValue("t"), LocalDateTime.now().toString(), LocalDateTime.now().toString());
 
-                String timeNow = LocalDateTime.now().format(App.DATE_TIME_FORMATTER);
+                String timeNow = LocalDateTime.now().toString();
                 LogItem logItem = new LogItem(taskNameCmd, timeNow, timeNow);
                 String[] lastLine = csvReader.readLastRowOfFile();
                 if((!lastLine[0].equals(projectNameCmd) && !lastLine[1].equals(taskNameCmd) )|| !lastLine[2].equals(lastLine[3])){
@@ -59,14 +58,13 @@ public class App {
                 CsvReader csvReader = new CsvReader();
                 Map<String, List<LogItem>> projectsMap = csvReader.readFile();
 
-
                 Optional<String> projectName = projectsMap.entrySet().stream().filter(x -> x.getValue().stream().anyMatch(y -> y.stopDateTime.equals(y.startDateTime))).findAny().map(x -> x.getKey());
                 Optional<String> taskName = projectsMap.entrySet().stream().filter(x -> x.getValue().stream().anyMatch(y -> y.stopDateTime.equals(y.startDateTime))).findAny().map(x -> x.getValue().get(0).taskName);
                 if (projectName.isPresent() && taskName.isPresent()) {
                     String pName = projectName.get();
                     String tName = taskName.get();
                     LogItem logItem = projectsMap.get(pName).stream().filter(x -> x.taskName.equals(tName)).findAny().get();
-                    logItem.stopDateTime = LocalDateTime.now().format(App.DATE_TIME_FORMATTER);
+                    logItem.stopDateTime = LocalDateTime.now().toString();
                     PrintWriter.saveEntry(pName, logItem);
                 }
             }
@@ -74,45 +72,16 @@ public class App {
             {
                 System.out.println(line.getOptionValue("project"));
                 Project project = new Project(line.getOptionValue("project"));
-                String timeNow = LocalDateTime.now().format(App.DATE_TIME_FORMATTER);
+                String timeNow = LocalDateTime.now().toString();
                 LogItem logItem = new LogItem(line.getOptionValue("task"), timeNow, timeNow);
                 PrintWriter.saveEntry(project.getName(), logItem);
-
-
             }
-
         }
+
         catch (ParseException exp) {
             // oops, something went wrong
             System.err.println("Parsing failed.  Reason: " + exp.getMessage());
-            formatter.printHelp("Usage: App [options]", options, true);
+            formatter.printHelp("App options in []", options, true);
         }
-
-//        Scanner scanner = new Scanner(System.in);
-//        Model model = new Model();
-//
-//        System.out.println("Welcome to the Time Pilot!");
-//
-//        // Prompt for one project name
-//        System.out.print("Enter the project name: ");
-//        String projectName = scanner.nextLine();
-//        Project project = new Project(projectName);
-//        model.addProject(project);
-//        System.out.println("Project '" + projectName + "' created.");
-//
-//        // Prompt for one task name
-//        System.out.print("Enter a task: ");
-//        String taskName = scanner.nextLine();
-//        project.addTask(taskName);
-//        System.out.println("Task '" + taskName + "' added to project '" + projectName + "'.");
-//
-//        // Show the project and its tasks
-//        System.out.println("\nProject Overview:");
-//        project.showProject();
-//
-//        System.out.println("Thank you for using our Time Pilot!");
-//
-//        scanner.close();
     }
 }
-
